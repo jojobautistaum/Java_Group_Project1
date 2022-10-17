dccb15288481218ad4ef849098214d8499555742
Jojo Bautista <gjojob@yahoo.com>
2022-10-17 17:55:15 -0500

    Updated starting files

---
+++ summative-1-jojo-yi-nara/src/main/java/com/project1/Summative1jojoyinara/Summative1JojoYiNaraApplication.java
@@ -0,0 +1,13 @@
+package com.project1.Summative1jojoyinara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1JojoYiNaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1JojoYiNaraApplication.class, args);
+	}
+
+}
